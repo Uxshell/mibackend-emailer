40a230f6e0663f3368b1737a5bf28f394646891e
Michelle <uxshell@gmail.com>
2020-12-16 23:43:37 -0600

    select

--- controllers/controlerLista.js
+++ controllers/controlerLista.js
@@ -55,7 +55,7 @@ const buscarLista = async(req, res = response) => {
     const select= req.params.seleccionada;
     let s = req.body.seleccionada;
    
-    console.log('valor de s'+s);
+    console.log('valor de s'+select);
     const lista = await Lista.find({nombre:select});
     const id = lista._id;
     console.log("LISTA"+ lista +"ID: "+id);
